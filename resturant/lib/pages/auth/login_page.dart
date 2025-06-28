c36610ae270046bd7b35db159ba83ce4010081ec
ritikegov <ritik.kumar@egovernments.org>
2025-06-28 17:51:51 +0530

    create login page

---
+++ resturant/lib/pages/auth/login_page.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:auto_route/auto_route.dart';
+import '../../core/constants.dart';
+import '../../core/utils.dart';
+import '../../bloc/auth_bloc.dart';
+import '../../app_router.dart';
+
+@RoutePage()
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        automaticallyImplyLeading: false,
+      ),
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          try {
+            if (state is AuthAuthenticated) {
+              AppUtils.showToast(context, AppConstants.successLoginSuccessful);
+              context.router.replaceAll([const TableViewRoute()]);
+            } else if (state is AuthError) {
+              AppUtils.showToast(context, state.message, isError: true);
+            } else if (state is AuthUnauthenticated) {
+              // Stay on login page
+            }
+          } catch (e) {
+            AppUtils.showToast(context, 'Navigation error: $e', isError: true);
+          }
+        },
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // App Logo/Title
+                  const Icon(
+                    Icons.restaurant,
+                    size: 80,
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    AppConstants.appName,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+
+                  // Username Field
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    validator: AppUtils.validateUsername,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  const SizedBox(height: 16),
+
+                  // Password Field
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText: !_isPasswordVisible,
+                    validator: AppUtils.validatePassword,
+                    textInputAction: TextInputAction.done,
+                    onFieldSubmitted: (_) => _handleLogin(),
+                  ),
+                  const SizedBox(height: 24),
+
+                  // Login Button
+                  BlocBuilder<AuthBloc, AuthState>(
+                    builder: (context, state) {
+                      final isLoading = state is AuthLoading;
+
+                      return ElevatedButton(
+                        onPressed: isLoading ? null : _handleLogin,
+                        child: isLoading
+                            ? const SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2,
+                                  valueColor: AlwaysStoppedAnimation<Color>(
+                                      Colors.white),
+                                ),
+                              )
+                            : const Text(
+                                'Login',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 16),
+
+                  // Signup Link
+                  TextButton(
+                    onPressed: () {
+                      try {
+                        context.router.push(const SignupRoute());
+                      } catch (e) {
+                        AppUtils.showToast(context, 'Navigation error: $e',
+                            isError: true);
+                      }
+                    },
+                    child: const Text(
+                      'Don\'t have an account? Sign up',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleLogin() {
+    try {
+      if (_formKey.currentState?.validate() ?? false) {
+        final username = _usernameController.text.trim();
+        final password = _passwordController.text;
+
+        context.read<AuthBloc>().add(
+              AuthLoginRequested(username: username, password: password),
+            );
+      }
+    } catch (e) {
+      AppUtils.showToast(context, 'Login error: $e', isError: true);
+    }
+  }
+}
