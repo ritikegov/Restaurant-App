188c9763dac3cb12b892009785a7fb1d96b0d6d5
ritikegov <ritik.kumar@egovernments.org>
2025-06-28 17:55:11 +0530

    add routing

---
+++ resturant/lib/app_router.dart
@@ -0,0 +1,53 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'pages/auth/login_page.dart';
+import 'pages/auth/signup_page.dart';
+import 'pages/table/table_view_page.dart';
+import 'pages/booking/booking_page.dart';
+import 'pages/menu/menu_page.dart';
+import 'pages/order/order_page.dart';
+import 'pages/order/order_history_page.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  RouteType get defaultRouteType => const RouteType.adaptive();
+
+  @override
+  List<AutoRoute> get routes => [
+        // Auth Routes
+        AutoRoute(
+          page: LoginRoute.page,
+          path: '/login',
+          initial: true,
+        ),
+        AutoRoute(
+          page: SignupRoute.page,
+          path: '/signup',
+        ),
+
+        // Main App Routes
+        AutoRoute(
+          page: TableViewRoute.page,
+          path: '/tables',
+        ),
+        AutoRoute(
+          page: BookingRoute.page,
+          path: '/booking',
+        ),
+        AutoRoute(
+          page: MenuRoute.page,
+          path: '/menu',
+        ),
+        AutoRoute(
+          page: OrderRoute.page,
+          path: '/order',
+        ),
+        AutoRoute(
+          page: OrderHistoryRoute.page,
+          path: '/order-history',
+        ),
+      ];
+}
---
+++ resturant/lib/app_router.gr.dart
@@ -0,0 +1,159 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'app_router.dart';
+
+abstract class _$AppRouter extends RootStackRouter {
+  // ignore: unused_element
+  _$AppRouter({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {
+    BookingRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const BookingPage(),
+      );
+    },
+    LoginRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const LoginPage(),
+      );
+    },
+    MenuRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const MenuPage(),
+      );
+    },
+    OrderHistoryRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const OrderHistoryPage(),
+      );
+    },
+    OrderRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const OrderPage(),
+      );
+    },
+    SignupRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const SignupPage(),
+      );
+    },
+    TableViewRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const TableViewPage(),
+      );
+    },
+  };
+}
+
+/// generated route for
+/// [BookingPage]
+class BookingRoute extends PageRouteInfo<void> {
+  const BookingRoute({List<PageRouteInfo>? children})
+      : super(
+          BookingRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'BookingRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [LoginPage]
+class LoginRoute extends PageRouteInfo<void> {
+  const LoginRoute({List<PageRouteInfo>? children})
+      : super(
+          LoginRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'LoginRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [MenuPage]
+class MenuRoute extends PageRouteInfo<void> {
+  const MenuRoute({List<PageRouteInfo>? children})
+      : super(
+          MenuRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MenuRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [OrderHistoryPage]
+class OrderHistoryRoute extends PageRouteInfo<void> {
+  const OrderHistoryRoute({List<PageRouteInfo>? children})
+      : super(
+          OrderHistoryRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'OrderHistoryRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [OrderPage]
+class OrderRoute extends PageRouteInfo<void> {
+  const OrderRoute({List<PageRouteInfo>? children})
+      : super(
+          OrderRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'OrderRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [SignupPage]
+class SignupRoute extends PageRouteInfo<void> {
+  const SignupRoute({List<PageRouteInfo>? children})
+      : super(
+          SignupRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SignupRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [TableViewPage]
+class TableViewRoute extends PageRouteInfo<void> {
+  const TableViewRoute({List<PageRouteInfo>? children})
+      : super(
+          TableViewRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'TableViewRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
